a66b8af728915ba886febc6b2ae8dee31d29f0f7
maskadev <sidkiyouness@gmail.com>
2022-04-27 23:18:46 +0000

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
+mix.browserSync('127.0.0.1:8000');
+// mix.ts('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css',);
 
-mix.ts('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+mix.ts("resources/js/app.js", "public/js")
+  .postCss("resources/css/app.css", "public/css", [
+    require("tailwindcss"),
+]);
