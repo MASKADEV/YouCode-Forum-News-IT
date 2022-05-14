11e3340efcf62c2fbd7c662b6a6e81fd845d96d1
maskadev <sidkiyouness@gmail.com>
2022-05-14 14:17:52 +0100

    add sass to laravel with tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,8 @@
 const mix = require('laravel-mix');
 mix.browserSync('127.0.0.1:8000');
-// mix.ts('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css',);
-
 
 mix.ts("resources/js/app.js", "public/js")
-  .postCss("resources/css/app.css", "public/css", [
-    require("tailwindcss"),
-]);
+.sass('resources/scss/app.scss', 'dist', {}, [
+  require("tailwindcss"),
+])
+.setPublicPath('web');
