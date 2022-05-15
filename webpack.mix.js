31714a3e7086859365536998c2c5c37d39e36c1a
maskadev <sidkiyouness@gmail.com>
2022-05-15 16:52:10 +0100

    delete scss from mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ const mix = require('laravel-mix');
 mix.browserSync('127.0.0.1:8000');
 
 mix.ts("resources/js/app.js", "public/js")
-.sass('resources/scss/app.scss', 'dist', {}, [
-  require("tailwindcss"),
-])
-.setPublicPath('web');
+  .postCss("resources/css/app.css", "public/css", [
+    require("tailwindcss"),
+]); 
