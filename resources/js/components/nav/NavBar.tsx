1c53be4f570a9ec7d100ec3be8234af36500cf6a
maskadev <sidkiyouness@gmail.com>
2022-05-15 16:54:19 +0100

    add "Add post" to navbar

--- resources/js/components/nav/NavBar.tsx
+++ resources/js/components/nav/NavBar.tsx
@@ -10,6 +10,7 @@ const Menu = () => {
               <h1 className='sm:text-2xl text-md font-bold w-[3rem] mx-4'>Brand.</h1>
               <Link className='px-3 ml-9 font-medium' href="/home">Home</Link>
               <Link className='px-3 ml-3 font-medium' href="/profile">Profile</Link>
+              <Link className='px-3 ml-3 font-medium' href="/add">Add Post</Link>
             </div>
             <div>
               <Link method='POST' href="/logout">Logout</Link>
