5dcfb4859123261f172cdc94a728fb5dbb5977be
maskadev <sidkiyouness@gmail.com>
2022-05-15 16:54:39 +0100

    add arrow icons for voting

--- resources/js/Pages/Home/Latest/Latest.tsx
+++ resources/js/Pages/Home/Latest/Latest.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../../../components/common/Layout';
 import {Head, Link} from '@inertiajs/inertia-react';
 
+
 interface props {
     'latestPosts' : Array<any>,
 }
@@ -10,42 +11,48 @@ const Latest: React.FC<props> = ({latestPosts}) => {
   return (
     <Layout>
         <Head title='Latest' />
-        <div>
+        <div className='w-full'>
+           <div className='sm:w-[60%] w-[90%] mx-auto'>
             <div className='flex flex-row'>
-                <Link href='/home/trending'><h1 className='mx-2 px-4 py-2 font-bold text-[2rem]'>Trending</h1></Link>
-                <Link href='/home'><h1 className='mx-2 px-4 py-2 font-bold text-[2rem] underline'>Latest</h1></Link>
-            </div>
-            <div >
-                {
-                latestPosts.length && latestPosts.map((element : any, index : number)=> {
-                    return  <div key={index} className="w-[90%] max-w-2xl bg-white font-[Poppins] py-7 px-9 mb-6 shadow-md rounded-xl">
-                                <div className="flex flex-row items-center justify-between h-[6rem]">
-                                    <div className="flex flex-col">
-                                        <h1 className="font-bold sm:text-2xl text-base w-[80%]">{element.title}</h1>
-                                        <p className="text-gray-400 text-[13px]">{element.created_at}</p>
-                                    </div>
-                                    <div className="flex flex-col justify-evenly items-center">
-                                        <p className="text-2xl font-bold font-[Poppins]">30</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    {element.image_url != null && <img src={`http://127.0.0.1:8000/uploads/images/${element.image_url}`} alt="" className='h-[300px]'/>}
-                                </div>
-                                <p className=" text-gray-400 mt-4"> {element.body} </p>
-                                <div className=" mt-5 flex flex-row justify-between">
-                                    <div className="flex flex-row justify-between w-full">
-                                        <div className="hover:cursor-pointer">
-                                            <Link href={`/postpreview/${element.id}`} className="ml-2">Comments</Link>
+                    <Link href='/home/trending'><h1 className='mx-2 px-4 py-2 font-bold text-[2rem]'>Trending</h1></Link>
+                    <Link href='/home'><h1 className='mx-2 px-4 py-2 font-bold text-[2rem] underline'>Latest</h1></Link>
+                </div>
+                <div >
+                    {
+                    latestPosts.length && latestPosts.map((element : any, index : number)=> {
+                        return  <div key={index} className="w-[90%] max-w-2xl bg-white font-[Poppins] py-7 px-9 mb-6 shadow-md rounded-xl">
+                                    <div className="flex flex-row items-center justify-between h-[6rem]">
+                                        <div className="flex flex-col">
+                                            <h1 className="font-bold sm:text-2xl text-base w-[80%]">{element.title}</h1>
+                                            <p className="text-gray-400 text-[13px]">{element.created_at}</p>
                                         </div>
-                                        <div className="sm:flex flex-row items-center text-gray-400 text-base hover:cursor-pointer">
-                                            <p className="">Autor</p>
+                                        <div className="flex flex-col justify-evenly items-center">
+                                        <img src="https://www.svgrepo.com/show/16833/up-arrow.svg" width="180"
+                                            height="180" className='w-[30px] cursor-pointer' alt="Up Arrow SVG Vector" title="Up Arrow SVG Vector" />
+                                            <p className="text-2xl font-bold font-[Poppins]">30</p>
+                                            <img src="https://www.svgrepo.com/show/145159/down-arrow.svg" 
+                                            className='w-[30px] cursor-pointer' alt="Down Arrow SVG Vector" title="Down Arrow SVG Vector" />
                                         </div>
                                     </div>
-                                </div>
-                        </div>
-                    })
-                }
-            </div>
+                                    <div>
+                                        {element.image_url != null && <img src={`http://127.0.0.1:8000/uploads/images/${element.image_url}`} alt="" className='h-[300px]'/>}
+                                    </div>
+                                    <p className=" text-gray-400 mt-4"> {element.body.split(" ")[0] + " " + element.body.split(" ")[1]} </p>
+                                    <div className=" mt-5 flex flex-row justify-between">
+                                        <div className="flex flex-row justify-between w-full">
+                                            <div className="hover:cursor-pointer">
+                                                <Link href={`/postpreview/${element.id}`} className="ml-2">Comments</Link>
+                                            </div>
+                                            <div className="sm:flex flex-row items-center text-gray-400 text-base hover:cursor-pointer">
+                                                <p className="">Autor</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        })
+                    }
+                </div>
+           </div>
         </div>
     </Layout>
   )
